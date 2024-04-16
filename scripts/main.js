e75a14f77cede225a5d3f7bd2312c1335b0b26d3
oscarh95 <oscarhou140@gmail.com>
2024-04-15 21:42:57 -0400

    revamped and updated series page

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,53 @@
-const dropdownBtn = document.getElementById("dropdown-btn")
-const dropdownContent = document.getElementById("dropdown-content")
-const dropdownContainer = document.getElementById("dropdown")
+const dropdownBtn = document.getElementById('dropdown-btn')
+const dropdownContent = document.getElementById('dropdown-content')
+const dropdownContainer = document.getElementById('dropdown')
 
+//Dropdown menu for season nav
 const toggleDropdown = () => {
-    dropdownContent.classList.toggle("show");
+    dropdownContent.classList.toggle('show');
 }
 
-dropdownBtn.addEventListener("mouseover", (e) => {
+dropdownBtn.addEventListener('mouseover', (e) => {
     e.stopPropagation();
     toggleDropdown();
 })
 
-dropdownContainer.addEventListener("mouseleave", (e) => {
-    if(dropdownContent.classList.contains("show")){
+dropdownContainer.addEventListener('mouseleave', (e) => {
+    if(dropdownContent.classList.contains('show')){
         toggleDropdown();
     }
 })
+
+//Fade out header when scrolling
+let didScroll
+let lastScrollTop = 0
+const scrollCheck = 5
+const navbarHeight = $('header').outerHeight()
+
+$(window).scroll((e) => {
+    didScroll = true
+})
+
+let headerScroll = () => {
+    let scrollPosition = $(this).scrollTop()
+    
+    if(Math.abs(lastScrollTop - scrollPosition) <= scrollCheck)
+        return
+
+    if(scrollPosition > lastScrollTop && scrollPosition > navbarHeight){
+        $('header').removeClass('nav-down').addClass('nav-up');
+    }
+    else{
+        if(scrollPosition + $(window).height() < $(document).height()){
+            $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    lastScrollTop = scrollPosition;
+}
+
+setInterval(() => {
+    if(didScroll){
+        headerScroll()
+        didScroll = false
+    }
+}, 250)
--- scripts/series.js
+++ scripts/series.js
@@ -1,26 +1,56 @@
-const thumbnailCollection =  document.getElementsByClassName("thumbnail")
+const thumbnailCollection =  document.querySelectorAll('.thumbnail')
+const playerCollection =  document.querySelectorAll('.visibility')
+const collapsibleCollection = document.querySelectorAll('.season-collapse')
+const seasonTabs = document.querySelectorAll('.season-number')
 
-for(let i = 0; i < thumbnailCollection.length; i++){
-    thumbnailCollection[i].addEventListener("click", () => {
-        thumbnailCollection[i].style.display = 'none';
-        loadYT();
-    })
-}
-
-
-
-loadYT = () => {
+const loadYT = () => {
     const tag = document.createElement('script');
     tag.src = "https://www.youtube.com/iframe_api";
     let firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    alert("API loaded")
 }
 
+let flag = false
+for(let i = 0; i < seasonTabs.length; i++){
+    seasonTabs[i].addEventListener("click", () => {
+        loadYT();
+        flag = true
+        console.log(flag)
+    })
+    if(flag){
+        break;
+    }
+}
+
+
+//Loop for showing video player hiding behind corresponding thumbnail
+for(let i = 0; i < thumbnailCollection.length; i++){
+    for(let j = 0; j < playerCollection.length; j++){
+        if(i == j){
+            thumbnailCollection[i].addEventListener("click", () => {
+                console.log(playerCollection[j])
+                thumbnailCollection[i].style.display = 'none';
+                playerCollection[j].classList.toggle('visibility-show')
+            })
+        }
+    }
+}
+
+//Array of videos (by season)
 const playerList = [
     {id:'player', height: '390', width: '640', videoId: '8dRRzOuKaeg'},
-    {id:'player2', height: '390', width: '640', videoId: '-ygZYfiDwH4'}
+    {id:'player2', height: '390', width: '640', videoId: '-ygZYfiDwH4'},
+    {id:'player3', height: '390', width: '640', videoId: '7WjZijQai40'},
+    {id:'player4', height: '390', width: '640', videoId: 'jz1tpqGD5EM'},
+    {id:'player5', height: '390', width: '640', videoId: '1emZAXvzi2g'},
+    {id:'player6', height: '390', width: '640', videoId: 'hosJ0_zoDtE'},
+    {id:'player7', height: '390', width: '640', videoId: '21TRHyA613g'},
+    {id:'player8', height: '390', width: '640', videoId: 'PmvzdMMu7ok'},
 ]
 
+
+//Loads Youtube Iframe + parameters using playerList array
 onYouTubeIframeAPIReady = () => {
     if(typeof playerList === 'undefined'){
         return
@@ -31,32 +61,28 @@ onYouTubeIframeAPIReady = () => {
                 width: player.width,
                 videoId: player.videoId,
                 playerVars: {
-                    'playsinline': 1
+                    'autoplay' : 0,
+                    'disablekb' : 1
                 },
-                events: {
-                    'onReady': onPlayerReady,
-                    'onStateChange': onPlayerStateChange
-            }
         });
     }
 
+    //Creates player using ids from playerList array
     for(let i = 0; i < playerList.length; i++){
         createPlayer(playerList[i])
     }
 }
 
-onPlayerReady = (e) => {
-    e.target.playVideo();
-}
-
-let done = false;
-onPlayerStateChange = (e) => {
-    if (e.data == YT.PlayerState.PLAYING && !done) {
-        setTimeout(stopVideo, 6000);
-        done = true;
-    }
-}
-
-stopVideo = () => {
-    player.stopVideo();
+//Collapsible sections
+for(let i = 0; i < collapsibleCollection.length; i++){
+    collapsibleCollection[i].addEventListener('click', () => {
+        collapsibleCollection[i].classList.toggle('active')
+        let mainContent = collapsibleCollection[i].nextElementSibling;
+        if(mainContent.style.maxHeight){
+            mainContent.style.maxHeight = null;
+        }
+        else{
+            mainContent.style.maxHeight = mainContent.scrollHeight + "px";
+        }
+    })
 }
