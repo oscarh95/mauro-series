afd308884a9e61bf794b7d84e427453a0c462ad6
oscarh95 <oscarhou140@gmail.com>
2024-04-19 01:07:52 -0400

    added seasons 2 - 4

--- scripts/series.js
+++ scripts/series.js
@@ -2,34 +2,27 @@ const thumbnailCollection =  document.querySelectorAll('.thumbnail')
 const playerCollection =  document.querySelectorAll('.visibility')
 const collapsibleCollection = document.querySelectorAll('.season-collapse')
 const seasonTabs = document.querySelectorAll('.season-number')
+const contentWrapper = document.getElementById('content-wrapper')
 
-const loadYT = () => {
+
+loadYT = () => {
     const tag = document.createElement('script');
     tag.src = "https://www.youtube.com/iframe_api";
     let firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-    alert("API loaded")
 }
 
-let flag = false
-for(let i = 0; i < seasonTabs.length; i++){
-    seasonTabs[i].addEventListener("click", () => {
-        loadYT();
-        flag = true
-        console.log(flag)
-    })
-    if(flag){
-        break;
-    }
-}
-
+document.addEventListener("DOMContentLoaded", () => {
+    contentWrapper.addEventListener("mouseover", () => {
+        loadYT()
+    }, {once:true})
+})
 
 //Loop for showing video player hiding behind corresponding thumbnail
 for(let i = 0; i < thumbnailCollection.length; i++){
     for(let j = 0; j < playerCollection.length; j++){
         if(i == j){
             thumbnailCollection[i].addEventListener("click", () => {
-                console.log(playerCollection[j])
                 thumbnailCollection[i].style.display = 'none';
                 playerCollection[j].classList.toggle('visibility-show')
             })
@@ -39,16 +32,39 @@ for(let i = 0; i < thumbnailCollection.length; i++){
 
 //Array of videos (by season)
 const playerList = [
-    {id:'player', height: '390', width: '640', videoId: '8dRRzOuKaeg'},
-    {id:'player2', height: '390', width: '640', videoId: '-ygZYfiDwH4'},
-    {id:'player3', height: '390', width: '640', videoId: '7WjZijQai40'},
-    {id:'player4', height: '390', width: '640', videoId: 'jz1tpqGD5EM'},
-    {id:'player5', height: '390', width: '640', videoId: '1emZAXvzi2g'},
-    {id:'player6', height: '390', width: '640', videoId: 'hosJ0_zoDtE'},
-    {id:'player7', height: '390', width: '640', videoId: '21TRHyA613g'},
-    {id:'player8', height: '390', width: '640', videoId: 'PmvzdMMu7ok'},
+    [
+        {id:'s1e1', height: '390', width: '640', videoId: '8dRRzOuKaeg'},
+        {id:'s1e2', height: '390', width: '640', videoId: '-ygZYfiDwH4'},
+        {id:'s1e3', height: '390', width: '640', videoId: '7WjZijQai40'},
+        {id:'s1e4', height: '390', width: '640', videoId: 'jz1tpqGD5EM'},
+        {id:'s1e5', height: '390', width: '640', videoId: '1emZAXvzi2g'},
+        {id:'s1e6', height: '390', width: '640', videoId: 'hosJ0_zoDtE'},
+        {id:'s1e7', height: '390', width: '640', videoId: '21TRHyA613g'},
+        {id:'s1e8', height: '390', width: '640', videoId: 'PmvzdMMu7ok'}
+    ],
+    [
+        {id:'s2e1', height: '390', width: '640', videoId: 'P_L-uaLQXgs'},
+        {id:'s2e2', height: '390', width: '640', videoId: 'GXOzUy-QKz8'},
+        {id:'s2e3', height: '390', width: '640', videoId: 'JhgsXl6eFOw'},
+        {id:'s2e4', height: '390', width: '640', videoId: '6qWOA53wVL0'}
+    ],
+    [
+        {id:'s3e1', height: '390', width: '640', videoId: '8qRWr6wZHXs'},
+        {id:'s3e2', height: '390', width: '640', videoId: 'X13AcMbCzKM'},
+        {id:'s3e3', height: '390', width: '640', videoId: 'QjWoZUWfGCE'},
+        {id:'s3e4', height: '390', width: '640', videoId: 'VqkU7bAUpE4'},
+        {id:'s3e5', height: '390', width: '640', videoId: '8a3QQM6O7'},
+        {id:'s3e6', height: '390', width: '640', videoId: 'U5gdUzVDrY0'},
+        {id:'s3e7', height: '390', width: '640', videoId: 'X1_QekvjT8E'},
+        {id:'s3e8', height: '390', width: '640', videoId: '2qKnIX7s2yk'},
+        {id:'s3e9', height: '390', width: '640', videoId: '4djhVRH-5Ac'},
+        {id:'s3e10', height: '390', width: '640', videoId: 'Dmcq-FQ0bP0'}
+    ],
+    [
+        {id:'s4e1', height: '390', width: '640', videoId: 'p7QZHNezmt4'},
+        {id:'s4e2', height: '390', width: '640', videoId: 'z8OCAWhWIpE'}
+    ]
 ]
-
 
 //Loads Youtube Iframe + parameters using playerList array
 onYouTubeIframeAPIReady = () => {
@@ -69,7 +85,8 @@ onYouTubeIframeAPIReady = () => {
 
     //Creates player using ids from playerList array
     for(let i = 0; i < playerList.length; i++){
-        createPlayer(playerList[i])
+        for(let j = 0; j < playerList[i].length; j++)
+            createPlayer(playerList[i][j])
     }
 }
 
